4b25266ba441c63835c93fa655b6378432d96a3b
NathanSchomer <nathan@nathanschomer.com>
2015-07-20 21:39:58 -0400

    Updated PSoC source file for lab3

--- lab3/PSoC4/lab3_PSoC4.c
+++ lab3/PSoC4/lab3_PSoC4.c
@@ -14,6 +14,18 @@
 #include <stdlib.h>
 #define MAX_BUFFER_SIZE 64
 
+int size(char *str1)
+{
+	int count = 0;
+	while(*str != '\0')
+	{
+		if(*str == ch)
+			return count;
+		count++; str++;
+	}
+	return -1;
+}
+
 void ReadString(char *buffer)
 {
     uint32 rxData;
@@ -67,53 +79,156 @@ int countBits(int v)
     return count;
 }
 
+//good
 int compareStrings(char *str1, char *str2)
 {
     while(*str1 != '\0' && *str2 != '\0')
     {
         if(*str1 != *str2)
-            return 0;
+            return *str1 - *str2;
         str1++; str2++;
     }
-    return 1;
+    return 0;
 }
+
 
 int searchForChar(char *str1, uint32 ch)
 {
+    int count = 0;
     while(*str1 != '\0')
     {
         if(*str1 == ch)
-            return 1;
-        str1++;
+            return count;
+        str1++; count++;
     }
-    return 0;
+    return -1;
 }
 
 int searchSubString(char *str1, char *subStr)
 {
-    char *original = subStr;
-    
-    while(*str1 != '\0')
-    {
-        while(*str1 == *subStr && *str1 != '\0'){ //If 
-             str1++; subStr++;
-             if(*subStr == '\0')
-                return 1;
-        }
-        subStr = original;  //reset subString pointer back to base address
-        str1++;
-    }
-    return 0;
+
+	char *str1_backup = str1;
+	int subsize = size(subStr);
+	int i;
+
+	//loop through all of str1
+	while(*str1 != 10){
+		if(*str1 == *subStr){
+			for(i = 1; i < subsize; i++){
+				if(*(str1 + i) != *(subStr + i))
+						break;
+			}
+			return str1 - str1_backup;
+		}
+		str1++;
+	}
+	return -1;
 }
 
-int main()
+int countWords(char *str)
 {
+	int count = 0;
+	int wordFound = 0;
+	while(*str != '\0'){
+		//find first char
+		if(*str != ' ' && wordFound == 0){
+			wordFound = 1;
+			count = 1;
+		}
+		else if(*str == ' ')
+			count++;
+		str++;
+	}
+	return count;
+}
+
+int longestWord(char *str){
+	int max = 0, maxWordBegin, maxWordEnd;
+	int currWordBegin = 0;
+	int i;
+
+	for(i = 0;; i++)
+	{
+		if(*(str+i) == ' ' || *(str+i) == '\0'){
+			if((i - currWordBegin) > max ){
+				max = i - currWordBegin;
+				maxWordBegin = currWordBegin;
+				maxWordEnd = --i;				//make sure this behaves as expected
+				if(*(str+i) == '\0')
+					break;
+			}
+			currWordBegin = i + 1;
+		}
+		return max;
+	}	
+
+int mostVowels(char *str){
+	int max = 0, curr = 0;
+
+	for(;;){
+		if(*str == ' ' || *str == '\0'){
+			if(curr > max)
+				max = curr;
+			if(*str == '\0')
+				return max;
+			curr = 0;
+		}
+		else
+		{
+			if(*str == 'a' || *str == 'A' ||
+			   *str == 'e' || *str == 'E' ||
+			   *str == 'i' || *str == 'I' || 
+			   *str == 'o' || *str == 'O' ||
+			   *str == 'u' || *str == 'U' )
+			{ 
+				curr++ 
+			}
+		}
+		str++;
+	}
+	return max;
+}
+
+int validateLuhn(int num)
+{
+	int sum = 0, i;
+	for(i = 1; num > 0; i += 2){
+		sum += num%power(10,i);
+		sum += 2*(num%power(10,i+1))%10 + 2*(num%power(10, i+1))/10;
+		if(num < 100) break;
+		else num /= 100;
+	}
+	if(sum % 10 == 0) return 1; //VALID
+	else return 0;				//INVALID
+}
+
+int power(int base, int exp){
+	int i, result = base;
+	for(i = 0; i < exp - 1; i++)
+		result *= base;
+	return result;
+}
+
+int checkPalindrome(char *word){
+	int length = size(word);
+	int i;
+
+	for(i = 0; i < (size(word)/2); i++){
+		if(*(word+i) != *(word+length-i-1))
+			return 0;	//palindrome
+	}
+	return 1; //not palindrome
+}
+
+
+
+int main(){
     ///// PART A
     CyGlobalIntEnable;      /* Enable global interrupts */
     UART_1_Start();
     
-    int choice;
-    int8 status;
+    int choice, input_num;
+    int8 status, status2, status3;
     uint32 ch, tmpChar;
     int count = 0;
     char str1[MAX_BUFFER_SIZE], str2[MAX_BUFFER_SIZE];
@@ -129,8 +244,7 @@ int main()
         {
             choice = tmpChar - '0';
             switch(choice){
-                case 1:
-                    //read in the strings and store them in str1 and str2
+                case 1: //compare strings
                     UART_1_UartPutString("\n \r Str1: ");
                     ReadString(str1);
                     UART_1_UartPutString("\n \r Str2: ");
@@ -139,8 +253,7 @@ int main()
                     WriteInt(status);
                 break;
                 
-                case 2:
-                    //read in the string ans store in str1. Read in char and store in ch
+                case 2: //check for char
                     UART_1_UartPutString("\n \r Str1: ");
                     ReadString(str1);
                     UART_1_UartPutString("\n \r Char: ");
@@ -154,8 +267,7 @@ int main()
                     WriteInt(status);
                 break;
                 
-                case 3:
-                    //Read in the string, store in str1. Read in substring and store in str2
+		case 3: //substring check
                     UART_1_UartPutString("\n \r Str1: ");
                     ReadString(str1);
                     UART_1_UartPutString("\n \r SubStr: ");
@@ -165,7 +277,40 @@ int main()
                     status = searchSubString(str1, str2);
                     WriteInt(status);
                 break;
-                
+		
+		case 4: //count, longest word, most vowels
+			UART_1_UartPutString("\n \r Str1:"); ReadString(str1);
+			ReadString(str1);
+
+			UART_1_UartPutString("\n \r Str2:"); ReadString(str2);
+			ReadString(str2);
+
+			//valid for the same string???
+			//add string output infront of return values
+			status = countWords(str1);
+		   	status2 = longestWord(str1);
+			status3 = mostVowels(str1);	
+			WriteInt(status);
+			WriteInt(status2);
+			WriteInt(status3);
+		break;
+
+		case 7: //luhn checksum validation
+			
+		//READ INT FROM TERMINAL
+		
+			status = validateLuhn(input_num));
+			WriteInt(status);
+		break;
+
+		case 8: //palindrome
+			UART_1_UartPutString("\n \r Str1: ");
+			ReadString(str1);
+
+			status = checkPalindrome(str1);			
+			WriteInt(status);
+		break;
+
                 default:
                 UART_1_UartPutString("\n \r Erroneous entry. Try again. \n \r");
             };
