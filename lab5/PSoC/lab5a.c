707df28ac26612767b4764e1b8c0c294045a157c
NathanSchomer <nathan@nathanschomer.com>
2015-08-11 08:26:16 -0400

    added LED control and improved passcode reset

--- lab5/PSoC/lab5a.c
+++ lab5/PSoC/lab5a.c
@@ -20,35 +20,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* comb = "24135"; //default password
+//char* comb = "24135"; //default password
+int comb = 24135;
+int tc_count = 0;
 //int currentState = -1;
 
 int attempts = 0;
 int permLock = 0;
-int unlocked = 0;
+int unlocked = 1;
+int blinkLed = 0;
+int ledOn = 0;
 
-void ResetPasscode()
-{
-    UART_1_UartPutString("Please reset passcod \n \r");
-}
-
-CY_ISR(uartISR){
-    //code to read and handle uart input
-    uint32 rxData;
-    rxData = UART_1_UartGetChar();
-    //UART_1_UartPutChar(rxData);      //echo back to terminal
+CY_ISR(timerISR){
+    //code to handle tc interrupt
     
-    if(rxData == '1'){
-        unlocked = 0;
-        UART_1_UartPutString("SYSTEM LOCEKD \n \r");
-    }
-    else if(rxData == 'c')
+    tc_count++;
+    
+    if(tc_count >= 250 && blinkLed == 1)
     {
-        ResetPasscode();
+        Pin_RED_Write(!(Pin_RED_Read()));
+        tc_count = 0;
     }
-    
-    //clear interrupt
-    UART_1_ClearRxInterruptSource(UART_1_GetRxInterruptSource());
+    else
+    {
+        Pin_RED_Write(unlocked);
+    }
+
+    TIMER_1_ReadStatusRegister();
 }
 
 int ReadInt()
@@ -72,6 +70,66 @@ int ReadInt()
     return result;
 }
 
+int ResetPasscode()
+{
+    //make LED blink
+    if(permLock == 0){
+        int tmp1, tmp2;
+        UART_1_UartPutString("Enter New Key: ");
+        tmp1 = ReadInt();
+        UART_1_UartPutString("\n\rEnter Key Again to Confirm: ");
+        tmp2 = ReadInt();
+        if(tmp1 == tmp2){
+            comb = tmp1;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+CY_ISR(uartISR){
+    uint32 rxData;
+    rxData = UART_1_UartGetChar();
+    //UART_1_UartPutChar(rxData);      //echo back to terminal
+    
+    if(rxData == '1' && unlocked == 1){
+        unlocked = 0;
+        UART_1_UartPutString("\n\rSYSTEM LOCEKD\n\r\n\r");
+    }
+    if(rxData == '2' && unlocked == 1)
+    {
+        blinkLed = 1;
+        if(ResetPasscode()){
+            unlocked = 0;
+            UART_1_UartPutString("\n\rSYSTEM LOCEKD\n\r\n\r");
+        }
+        else
+        {
+            UART_1_UartPutString("\n\rKey Mismatch!\n\r");
+        }
+        blinkLed = 0;
+    }
+    
+    //clear interrupt
+    UART_1_ClearRxInterruptSource(UART_1_GetRxInterruptSource());
+}
+
+uint32 GetChar()
+{
+    uint32 rxData;
+    for(;;)
+    {
+        rxData = UART_1_UartGetChar();
+        if(rxData){
+            if(rxData == '\r') break;
+            UART_1_UartPutChar(rxData);
+            return rxData;
+        }
+    }
+    
+    return rxData;
+}
+
 void ReadString(char *buffer)
 {
     uint32 rxData;
@@ -83,6 +141,7 @@ void ReadString(char *buffer)
         a char where 1 to 255 are valid ASCII characters and 0 indicates an error
         or no data present.*/
         rxData = UART_1_UartGetChar();
+        
         if(rxData){
             UART_1_UartPutChar(rxData);//echo char back to terminal
             if(rxData == '\r')
@@ -99,40 +158,41 @@ void ReadString(char *buffer)
 
 int getCombination()
 {
-	char key1 = comb[0];
+    //UART_1_UartPutString("getCombination");
+	//char key1 = comb[0];
 	int currentState = -1;
-	char input;
-	while(1){
+	int input;
+	for(;;){
 		//get button pushed by user via UART
-		input = UART_1_UartGetChar();
-		while(input == '\n')
-			input = getchar();
+		input = GetChar();
+		while(input == '\r')
+			input = UART_1_UartGetChar();
 			
-		if(input == comb[0]){
+		if(input == (comb/10000)%10+'0'){
 			if(currentState == -1)
 				currentState = 1;
 			else
 				return 0;
 		}
-		else if(input == comb[1]){
+		else if(input == (comb/1000)%10+'0'){
 			if(currentState == 1)
 				currentState = 2;
 			else
 				return 0;
 		}
-		else if(input == comb[2]){
+		else if(input == (comb/100)%10+'0'){
 			if(currentState == 2)
 				currentState = 3;
 			else
 				return 0;
 		}
-		else if(input == comb[3]){
+		else if(input == (comb/10)%10+'0'){
 			if(currentState == 3)
 				currentState = 4;
 			else
 				return 0;
 		}
-		else if(input == comb[4]){
+		else if(input == (comb)%10+'0'){
 			if(currentState == 4)
 				return 1;
 			else
@@ -150,27 +210,34 @@ int main(){
     
     CyGlobalIntDisable;
     UART_1_UartPutString("Established connection to terminal. \n \r"); 
-    UART_1_UartPutString("Combination Lock \n \r");
+    UART_1_UartPutString("Combination Lock \n\r\n\r");
 	Uart_Int_Start();
     Uart_Int_SetVector(uartISR);
+    
+    myTimer_Int_StartEx(timerISR);
     CyGlobalIntEnable;
     
-    char key1 = comb[0];
+    TIMER_1_Start();
+    
+    //char key1 = comb[0];
     char input;
         
     for(;;){
-		if(permLock == 1){}
-		else if(permLock == 0 && attemps >= 3){
+        
+		if(permLock == 1 || unlocked == 1){}
+		else if(permLock == 0 && attempts >= 3){
 			permLock = 1;
 			UART_1_UartPutString("\n\rToo Many Attempts!\n\rPerm Lock Set \n \r");
 		}
 		else if(attempts < 3 && getCombination()){
 			unlocked = 1;
-			UART_1_UartPutString("ACCESS GRANTED \n \r");
+            attempts = 0;
+            
+			UART_1_UartPutString("\n\rACCESS GRANTED\n\r\n\r");
 		}
 		else if(attempts < 3){
 			attempts++;
-			UART_1_UartPutString("ACCESS DENIED \n \r");
+			UART_1_UartPutString("\n\rACCESS DENIED\n\r\n\r");
 		}
     }
 }
